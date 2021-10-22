5053c21e9910b3420e2d4c02b665212d8726f8a7
javierOrt <javito19991@gmail.com>
2021-10-22 20:01:59 +0200

    Primer dia de PM y DM
    soy feliz

--- Proyectos/T03_inicio/app/src/main/java/com/example/t03_inicio/MainActivity.java
+++ Proyectos/T03_inicio/app/src/main/java/com/example/t03_inicio/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.t03_inicio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button saludar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+        saludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"VIVA ANDROID MMG",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void instancias() {
+        saludar = findViewById(R.id.boton_saludar);
     }
 }
