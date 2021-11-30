8d5df8bf52e53e5d01ef2930236ba7687bc1c251
javierOrt <javito19991@gmail.com>
2021-11-30 17:36:28 +0100

    pp

---
+++ Proyectos/listas/app/src/main/java/com/example/listas/utils/Marcas.java
@@ -0,0 +1,32 @@
+package com.example.listas.utils;
+
+public class Marcas {
+    private String nombre;
+    private int imagen;
+
+    public Marcas(String nombre, int imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
